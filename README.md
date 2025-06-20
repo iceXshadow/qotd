f50c440cbfcff3508f317b6ee7a1f854e9f23d38
iceXshadow <devxicy@gmail.com>
2025-06-20 16:58:36 +0000

    Update quote of the day

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Quote of the Day
 
 <!-- QUOTE_START -->
-> It is better to disappoint people with the truth than to appease them with a lie.
+> Cultivate the merry heart, develop the happiness habit, and life will become a continual feast.
 >
-> — Simon Sinek
+> — Norman Vincent Peale
 
 Inspirational quotes provided by <a href="https://zenquotes.io/" target="_blank">ZenQuotes API</a>
 <!-- QUOTE_END -->
--- quotes.py
+++ quotes.py
@@ -1,4 +1,5 @@
 quotes = [
+    "Cultivate the merry heart, develop the happiness habit, and life will become a continual feast.",
     "It is better to disappoint people with the truth than to appease them with a lie.",
     "Focus on how far you have come in life rather than looking at the accomplishments of others.",
     "Determine never to be idle. No person will have occasion to complain of the want of time who never loses any.",
